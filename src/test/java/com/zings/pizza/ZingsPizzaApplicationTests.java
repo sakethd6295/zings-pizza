323ef921856dba96fb193eb82f64a4822dafd7ea
Saketh Reddy  Dumpala <sakethdumpala@Sakeths-Air.lan>
2022-03-21 23:26:17 -0500

    project setup updates

---
+++ src/test/java/com/zings/pizza/ZingsPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zings.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZingsPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
